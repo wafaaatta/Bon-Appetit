a6f68000e9ba77a89ac396f7a4dd8050a5deda2e
chrislaf63 <chrislaf63@gmail.com>
2024-08-28 15:08:11 +0200

    Correction category

--- backend/database/migrations/2024_08_28_114436_create_category_recipe_table.php
+++ backend/database/migrations/2024_08_28_114436_create_category_recipe_table.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categorie_recipe');
+        Schema::dropIfExists('category_recipe');
     }
 };
