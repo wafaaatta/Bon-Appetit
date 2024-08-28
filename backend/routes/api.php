7e6c19cc1c749fc6d30183e002fe3f4fceeb5670
JohanPires <johan2408.jp@gmail.com>
2024-08-28 16:35:19 +0200

    commit fin de journée

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\RecipesController;
 
 // Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 //     return $request->user();
@@ -13,3 +14,10 @@ Route::get('/user/{id}', [UserController::class, 'getOneUser']);
 Route::post('/user', [UserController::class, 'postUser']);
 Route::put('/user/{id}', [UserController::class, 'editUser']);
 Route::delete('/user/{id}', [UserController::class, 'deleteUser']);
+
+Route::get('/recipes', [RecipesController::class, 'getRecipes']);
+Route::get('/recipes/{id}', [RecipesController::class, 'getRecipe']);
+Route::post('/recipes', [RecipesController::class, 'postRecipe']);
+Route::put('/recipes/{id}', [RecipesController::class, 'editRecipe']);
+Route::delete('/recipes/{id}', [RecipesController::class, 'deleteRecipe']);
+
