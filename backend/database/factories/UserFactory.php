bff09c6c9d4e60031147dc1d8575cb834ff162cd
chrislaf63 <chrislaf63@gmail.com>
2024-08-28 16:32:50 +0200

    ajouts RecipeFactory et seeders

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -24,9 +24,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'role' => fake()->randomElement(['admin', 'user']),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
