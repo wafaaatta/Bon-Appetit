703aff69debf3880b942042231c3ff27a6e8c9af
SalahTazoukanit <salahtazu1996@gmail.com>
2024-08-28 14:40:56 +0200

    header && views

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,45 @@
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <div className="bg-third-color flex justify-center items-center gap-32">
+        <div className="flex items-center gap-32 w-11/12">
+          <div>
+            <img
+              className="w-32"
+              src="/src/assets/images-ba/logo-bon-appetit.png"
+              alt="image logo Bon Appetit"
+            />
+          </div>
+          <div className="flex justify-center items-center gap-24 text-first-color">
+            <NavLink
+              className={(nav) => (nav.isActive ? "nav-active" : "")}
+              to={"/"}
+            >
+              Accueil
+            </NavLink>
+            <NavLink
+              className={(nav) => (nav.isActive ? "nav-active" : "")}
+              to={"/recipes"}
+            >
+              Recettes
+            </NavLink>
+            {
+              <NavLink
+                className={(nav) => (nav.isActive ? "nav-active" : "")}
+                to={"/profile"}
+              >
+                Mon Profile
+              </NavLink>
+            }
+            <div className="btn flex justify-center items-center rounded text-white bg-first-color">
+              <NavLink to={""}>Connexion</NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Header;
