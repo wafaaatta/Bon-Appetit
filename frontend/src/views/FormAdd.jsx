91e6524abe7d90048de3ec37fb5685b1910ff5a1
SalahTazoukanit <salahtazu1996@gmail.com>
2024-08-30 16:39:12 +0200

    form add almost finished

--- frontend/src/views/FormAdd.jsx
+++ frontend/src/views/FormAdd.jsx
@@ -1,31 +1,28 @@
 import axios from "axios";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const FormAdd = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [picture, setPicture] = useState(null);
+  const [category, setCategory] = useState("");
+
   const [user_id, setUserId] = useState("1");
+
   const [recipe_id, setRecipeId] = useState("");
 
-  // setFile(URL.createObjectURL(e.target.files[0]));
+  const [ingredients, setIngredients] = useState("");
+  const [ingredient_name, setIngridientName] = useState("");
 
   const addRecipeInfos = (e) => {
     e.preventDefault();
 
-    console.log(user_id);
-    console.log(content);
-    console.log(picture);
-    console.log(title);
-
-    // e.target.files[0];
-
     const recipe = new FormData();
     recipe.append("title", title);
     recipe.append("content", content);
-    // recipe.append("picture", picture); // Ajouter l'image
+    recipe.append("category", category);
     recipe.append("user_id", user_id);
 
     if (picture) {
@@ -36,7 +33,7 @@ const FormAdd = () => {
     const headers = {
       "Content-Type": "multipart/form-data",
     };
-    // Envoyer les données via axios
+
     axios
       .post("http://127.0.0.1:8000/api/recipes", recipe, {
         headers,
@@ -47,6 +44,33 @@ const FormAdd = () => {
         setRecipeId(recipe_id);
       });
   };
+
+  const addIngridientName = (e) => {
+    e.preventDefault();
+
+    const ingredient = {
+      name: ingredient_name,
+    };
+
+    axios
+      .post("http://127.0.0.1:8000/api/ingredients/" + recipe_id, ingredient)
+      .then((response) => {
+        console.log(response);
+      })
+      .then(() => {
+        getIngredientRecipe();
+      });
+
+    const getIngredientRecipe = () => {
+      axios
+        .get("http://127.0.0.1:8000/api/recipes/" + recipe_id)
+        .then((response) => {
+          console.log(response);
+          setIngredients(response.data.ingredient);
+        });
+    };
+  };
+
   return (
     <>
       <Header />
@@ -98,33 +122,55 @@ const FormAdd = () => {
                   required
                 />
               </div>
+              <div className="flex ml-4 mt-10">
+                <h4>Categories :</h4>
+                <select name="categories" id="">
+                  <option name="category" value="1">
+                    Entrée
+                  </option>
+                  <option name="category" value="2">
+                    Plat
+                  </option>
+                  <option name="category" value="3">
+                    Dessert
+                  </option>
+                </select>
+              </div>
             </div>
           </form>
-          {/* {recipe_id && ( */}
-          <div className="flex flex-col gap-3 ml-4 mt-5">
-            <div className="flex">
-              <h3>Ingredients :</h3>
+          {recipe_id && (
+            <div className="flex flex-col gap-3 ml-4 mt-5">
+              <div className="flex">
+                <h3>Ingredients :</h3>
+              </div>
+              <div className="flex flex-wrap gap-1">
+                {ingredients &&
+                  ingredients.map((ingredient) => (
+                    <div
+                      key={ingredient.id}
+                      className="flex items-center gap-6 text-gray-400"
+                    >
+                      {ingredient.name}
+                    </div>
+                  ))}
+              </div>
+              <form onSubmit={addIngridientName} className="flex w-2/5 gap-3">
+                <input
+                  onChange={(e) => setIngridientName(e.target.value)}
+                  className="input-text"
+                  type="text"
+                  name="name"
+                  placeholder="ingredient"
+                />
+                <button
+                  className="btn bg-second-color rounded text-white"
+                  type="submit"
+                >
+                  Ajouter +
+                </button>
+              </form>
             </div>
-            <div className="flex gap-1"></div>
-            <form className="flex w-2/5 gap-3">
-              <input
-                className="input-text"
-                type="text"
-                name="name"
-                placeholder="ingredient"
-              />
-              <button
-                className="btn bg-second-color rounded text-white"
-                type="submit"
-              >
-                Ajouter +
-              </button>
-            </form>
-          </div>
-          {/* )} */}
-          <div className="flex ml-4 mt-10">
-            <h4>Categories :</h4>
-          </div>
+          )}
           <div className="flex justify-center ml-4 mt-10 mb-10 bg-first-color w-40 h-10 rounded-full text-white">
             <button>Ajouter Recette</button>
           </div>
