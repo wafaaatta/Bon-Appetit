7a2a1bdc52f390c59adde24e18ba0b74c23131c3
SalahTazoukanit <salahtazu1996@gmail.com>
2024-08-30 15:21:42 +0200

    adding recipe

--- frontend/src/views/FormAdd.jsx
+++ frontend/src/views/FormAdd.jsx
@@ -1,7 +1,52 @@
+import axios from "axios";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import { useState } from "react";
 
 const FormAdd = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [picture, setPicture] = useState(null);
+  const [user_id, setUserId] = useState("1");
+  const [recipe_id, setRecipeId] = useState("");
+
+  // setFile(URL.createObjectURL(e.target.files[0]));
+
+  const addRecipeInfos = (e) => {
+    e.preventDefault();
+
+    console.log(user_id);
+    console.log(content);
+    console.log(picture);
+    console.log(title);
+
+    // e.target.files[0];
+
+    const recipe = new FormData();
+    recipe.append("title", title);
+    recipe.append("content", content);
+    // recipe.append("picture", picture); // Ajouter l'image
+    recipe.append("user_id", user_id);
+
+    if (picture) {
+      recipe.append("picture", picture);
+    }
+    console.log(picture);
+
+    const headers = {
+      "Content-Type": "multipart/form-data",
+    };
+    // Envoyer les données via axios
+    axios
+      .post("http://127.0.0.1:8000/api/recipes", recipe, {
+        headers,
+      })
+      .then((response) => {
+        console.log(response);
+        const recipe_id = response.data.recipe_id;
+        setRecipeId(recipe_id);
+      });
+  };
   return (
     <>
       <Header />
@@ -11,52 +56,77 @@ const FormAdd = () => {
           <div className="flex ml-4 mt-10">
             <h2 className="font-bold">Ajouter Recette</h2>
           </div>
-          <div className="flex flex-row gap-4 w-full ml-4 mt-10">
+          <form
+            onSubmit={(e) => addRecipeInfos(e)}
+            className="flex flex-row gap-4 w-full ml-4 mt-10"
+            encType="multipart/form-data"
+          >
             <div className="flex flex-col w-2/5 gap-3">
               <div>
                 Nom: <br />
                 <input
+                  onChange={(e) => setTitle(e.target.value)}
                   className="input-text"
                   type="text"
                   placeholder="Inserez un nom"
+                  name="title"
+                  required
                 />
               </div>
               <div>
-                Ingredients: <br />
+                Preparations: <br />
                 <textarea
-                  placeholder="Inserez les ingredients"
-                  name=""
+                  onChange={(e) => setContent(e.target.value)}
+                  placeholder="Inserez la preparation"
+                  name="content"
                   id=""
+                  required
                 ></textarea>
+              </div>
+              <div className="flex border justify-center bg-second-color text-white rounded">
+                <button type="submit">Ajouter Ingredient +</button>
               </div>
             </div>
             <div className="flex flex-col  w-2/5 gap-3">
               <div>
-                Preparations: <br />
-                <textarea
-                  placeholder="Inserez la preparation"
-                  name=""
+                Ajouter une image: <br />
+                <input
+                  onChange={(e) => setPicture(e.target.files[0])}
+                  type="file"
+                  name="picture"
                   id=""
-                ></textarea>
+                  required
+                />
               </div>
             </div>
-          </div>
-          <div></div>
-          <div>
-            <div className="flex w-2/5 gap-3">
+          </form>
+          {/* {recipe_id && ( */}
+          <div className="flex flex-col gap-3 ml-4 mt-5">
+            <div className="flex">
+              <h3>Ingredients :</h3>
+            </div>
+            <div className="flex gap-1"></div>
+            <form className="flex w-2/5 gap-3">
               <input
                 className="input-text"
                 type="text"
+                name="name"
                 placeholder="ingredient"
               />
-              <button>ajouter ingredient +</button>
-            </div>
+              <button
+                className="btn bg-second-color rounded text-white"
+                type="submit"
+              >
+                Ajouter +
+              </button>
+            </form>
           </div>
+          {/* )} */}
           <div className="flex ml-4 mt-10">
             <h4>Categories :</h4>
           </div>
           <div className="flex justify-center ml-4 mt-10 mb-10 bg-first-color w-40 h-10 rounded-full text-white">
-            <button>Ajouter</button>
+            <button>Ajouter Recette</button>
           </div>
         </div>
       </div>
