3a91c2ab822b922f61ea2b7e2b1ca867bbcfada1
chrislaf63 <125891353+chrislaf63@users.noreply.github.com>
2024-09-02 13:09:53 +0200

    Ajout Login et Register

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,6 +16,8 @@ Route::get('/user/{id}', [UserController::class, 'getOneUser']);
 Route::post('/user', [UserController::class, 'postUser']);
 Route::put('/user/{id}', [UserController::class, 'editUser']);
 Route::delete('/user/{id}', [UserController::class, 'deleteUser']);
+Route::post('/user/login', [UserController::class, 'login']);
+Route::post('/user/register', [UserController::class, 'postUser']);
 
 Route::get('/recipes', [RecipesController::class, 'getRecipes']);
 Route::get('/recipes/{id}', [RecipesController::class, 'getRecipe']);
@@ -23,9 +25,9 @@ Route::post('/recipes', [RecipesController::class, 'postRecipe']);
 Route::put('/recipes/{id}', [RecipesController::class, 'editRecipe']);
 Route::delete('/recipes/{id}', [RecipesController::class, 'deleteRecipe']);
 
-Route::get('recipes/{recipeId}/comments', [CommentsController::class, 'getComments']);
-Route::post('recipes/{recipeId}/comments', [CommentsController::class, 'postComment']);
-Route::delete('comments/{id}', [CommentsController::class, 'deleteComment']);
+Route::get('/recipes/{recipeId}/comments', [CommentsController::class, 'getComments']);
+Route::post('/recipes/{recipeId}/comments', [CommentsController::class, 'postComment']);
+Route::delete('/comments/{id}', [CommentsController::class, 'deleteComment']);
 Route::post('/attachIngredient/{id}', [RecipesController::class, 'attachIngredient']);
 
 Route::get('/ingredients', [IngredientController::class, 'getIngredients']);
